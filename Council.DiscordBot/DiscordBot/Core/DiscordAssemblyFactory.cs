9b60b1442df399936edae03c4f42a6514669f112
David Colclazier <jdcolclazier@gmail.com>
2023-11-05 23:49:56 -0700

    fix command location

--- Council.DiscordBot/DiscordBot/Core/DiscordAssemblyFactory.cs
+++ Council.DiscordBot/DiscordBot/Core/DiscordAssemblyFactory.cs
@@ -1,4 +1,6 @@
+using AWS.Logging;
 using Council.DiscordBot.Contract;
+using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
 using System.Composition;
 using System.IO;
@@ -9,27 +11,18 @@ using System.Reflection.Metadata.Ecma335;
 namespace Council.DiscordBot.Core
 {
     [Export(typeof(IAssemblyFactory))]
-    public class DiscordAssemblyFactory : IAssemblyFactory
+    public class DiscordAssemblyFactory : LoggingResource, IAssemblyFactory
     {
         private static readonly string[] assemblies = {
-            $"DiscordBot.Commands.dll",
+            $"/opt/discordbot/DiscordBot.Commands.dll",
         };
 
         private static readonly string _assemblyPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 
         //todo - pull assemblies from s3 bucket for pluggability
-        public IEnumerable<string> Assemblies()
-        {
-            var list = new List<string>();
-            foreach(var assembly in assemblies)
-            {
-                list.AddRange(Directory.GetFiles(_assemblyPath, assembly));
-            }
-            return list.AsReadOnly();
-        }
+        public IEnumerable<string> Assemblies() => assemblies.ToList().AsReadOnly();
 
-
-        public DiscordAssemblyFactory() { }
+        public DiscordAssemblyFactory(): base(nameof(DiscordAssemblyFactory)) { }
     }
 
 }
