6b290004c3f6b7f6716fe948fb6cd285d809ed6e
David Colclazier <jdcolclazier@gmail.com>
2023-11-05 22:51:30 -0700

    Fix assembly paths to commands.

--- Council.DiscordBot/DiscordBot/Core/DiscordAssemblyFactory.cs
+++ Council.DiscordBot/DiscordBot/Core/DiscordAssemblyFactory.cs
@@ -1,25 +1,32 @@
 using Council.DiscordBot.Contract;
 using System.Collections.Generic;
 using System.Composition;
+using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Reflection.Metadata.Ecma335;
 
 namespace Council.DiscordBot.Core
 {
     [Export(typeof(IAssemblyFactory))]
     public class DiscordAssemblyFactory : IAssemblyFactory
     {
-#if RELEASE
         private static readonly string[] assemblies = {
-            $".\\DiscordBot.Commands.dll",
+            $"DiscordBot.Commands.dll",
         };
-#else
-        private static readonly string[] assemblies = {
-            @"C:\Development\MVP.452\MVP.DiscordBot\bin\DiscordBot.Commands.dll",
-        };
-#endif
+
+        private static readonly string _assemblyPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 
         //todo - pull assemblies from s3 bucket for pluggability
-        public IEnumerable<string> Assemblies() => assemblies.ToList().AsReadOnly();
+        public IEnumerable<string> Assemblies()
+        {
+            var list = new List<string>();
+            foreach(var assembly in assemblies)
+            {
+                list.AddRange(Directory.GetFiles(_assemblyPath, assembly));
+            }
+            return list.AsReadOnly();
+        }
 
 
         public DiscordAssemblyFactory() { }
